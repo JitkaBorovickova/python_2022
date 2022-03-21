ccc9cd89774129eac86c5ba565d324cbf6f3b761
Jitka Borovickova <jitka.borovickova@icloud.com>
2022-03-21 20:29:53 +0100

    confirnn

---
+++ ukol_1.py
@@ -0,0 +1,20 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+balik = input("Zadejte cislo baliku:")
+delivery = baliky[balik]
+if delivery == True:
+  print('Balik byl predan kuryrovi.')
+else:
+  print('Balik zatim nebyl predan kuryrovi.')
+
+
+
+
+
+
